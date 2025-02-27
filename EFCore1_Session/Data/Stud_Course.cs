1d430dcd45d39f9c9f3e8d23fed39009c250aeec
Youssef El sherbiny <149201516+Elsherbiny73@users.noreply.github.com>
2025-02-27 12:02:55 +0200

    Add files via upload

---
+++ EFCore1_Session/Data/Stud_Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskSessionEFCore1.Data
+{
+    public  class Stud_Course
+    {
+        [Key]
+        public int Stud_Id {  get; set; }
+        public int Course_Id {  get; set; }
+        public int Grade {  get; set; }
+
+    }
+}
---
+++ EFCore1_Session/Data/Topic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskSessionEFCore1.Data
+{
+    public class Topic
+    {
+        public int Id { get; set; } 
+        public string? Name { get; set; }    
+    }
+}
