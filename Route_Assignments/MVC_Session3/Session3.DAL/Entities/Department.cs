881d644248c1fdf51f23f504d728c130a39894af
Youssef El sherbiny <149201516+Elsherbiny73@users.noreply.github.com>
2025-04-07 11:22:40 -0700

    Delete Route_Assignments/MVC_Session3 directory
    delete old MVC_Session3

--- Route_Assignments/MVC_Session3/Session3.DAL/Entities/Department.cs
+++
@@ -1,9 +0,0 @@
-namespace Session3.DAL.Entities;
-
-public class Department: ModelBas
-{
-    public string Name { get; set; } = null!;
-    public string Description { get; set; } = null!;
-    public string Code { get; set; } = null!;
-    public DateOnly CreationDate{ get; set; }
-}
--- Route_Assignments/MVC_Session3/Session3.PL/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Session3.PL.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
