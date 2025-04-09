00394593aabb5c4bbd29cfc072a8b89ce78bfc2e
Youssef El sherbiny <149201516+Elsherbiny73@users.noreply.github.com>
2025-04-09 03:30:13 +0200

    Add files via upload

---
+++ Route_Assignments/MVC_Session4/Company.Web/Controllers/EmployeeController.cs
@@ -0,0 +1,59 @@
+﻿using Company.Service.Interfaces;
+using Company.Service.Interfaces.Employee.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company.Web.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeServices _employeeServices;
+        private readonly IDepartmentServices _departmentServices;
+
+        public EmployeeController(IEmployeeServices employeeServices, IDepartmentServices departmentServices)
+        {
+            _employeeServices = employeeServices;
+            _departmentServices = departmentServices;
+        }
+        //[HttpGet]
+        public IActionResult Index(string searchInp)
+        {
+            IEnumerable<EmployeeDto> employees = new List<EmployeeDto>();
+
+            if (string.IsNullOrEmpty(searchInp))
+                employees = _employeeServices.GetAll();
+            else
+                employees = _employeeServices.GetEmployeeByName(searchInp);
+    
+            return View(employees);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.Departments = _departmentServices.GetAll();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(EmployeeDto employee)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _employeeServices.Add(employee);
+                    return RedirectToAction(nameof(Index));
+                }
+
+                ViewBag.Departments = _departmentServices.GetAll(); 
+                ModelState.AddModelError("Employee", "ValidationErrors");
+                return View(employee);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Departments = _departmentServices.GetAll();
+                ModelState.AddModelError("Employee", ex.Message);
+                return View(employee);
+            }
+        }
+    }
+}
