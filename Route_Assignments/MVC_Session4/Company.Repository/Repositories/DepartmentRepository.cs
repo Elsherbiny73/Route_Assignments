8d0282acc11292957d3360567bbf63049ea93b2b
Youssef El sherbiny <149201516+Elsherbiny73@users.noreply.github.com>
2025-04-09 03:26:12 +0200

    Add files via upload

---
+++ Route_Assignments/MVC_Session4/Company.Repository/Repositories/DepartmentRepository.cs
@@ -0,0 +1,15 @@
+﻿using Company.Data.Contexts;
+using Company.Data.Entites;
+using Company.Repository.Interfaces;
+
+namespace Company.Repository.Repositories
+{
+    public class DepartmentRepository : GenericRepository<Department> ,IDepartmentRepository
+    {
+        public DepartmentRepository(CompanyDbContext context) : base(context) 
+        {
+
+        }
+        
+    }
+}
---
+++ Route_Assignments/MVC_Session4/Company.Repository/Repositories/GenericRepository.cs
@@ -0,0 +1,41 @@
+﻿using Company.Data.Contexts;
+using Company.Data.Entites;
+using Company.Repository.Interfaces;
+
+namespace Company.Repository.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly CompanyDbContext _context;
+        public GenericRepository(CompanyDbContext context)
+        {
+            _context = context;
+        }
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            _context.SaveChanges(); 
+        }
+        public void Delete(T entity)
+        {
+            var existing = _context.Set<T>().Find(entity.Id); 
+
+            if (existing != null)
+            {
+                _context.Set<T>().Remove(existing);
+                _context.SaveChanges();
+            }
+        }
+        public IEnumerable<T> GetAll()
+            => _context.Set<T>().ToList();
+
+        public T GetById(int id)
+            => _context.Set<T>().Find(id);
+
+        public void Update(T entity) 
+        {
+            _context.Set<T>().Update(entity);
+            _context.SaveChanges();
+        }
+    }
+}
