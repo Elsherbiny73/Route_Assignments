d9ed2d5e7abebfdafeb9b40056368324c843c286
Youssef El sherbiny <149201516+Elsherbiny73@users.noreply.github.com>
2025-04-10 03:33:20 +0200

    Add files via upload

---
+++ Route_Assignments/MVC_Session5/Company.Repository/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,9 @@
+﻿using Company.Data.Entites;
+namespace Company.Repository.Interfaces
+{
+    public interface IEmployeeRepository : IGenericRepository<Employee>
+    {
+        IEnumerable<Employee> GetEmployeeByName(string name);
+        IEnumerable<Employee> GetEmployeesByAddress(string address);
+    }
+}
---
+++ Route_Assignments/MVC_Session5/Company.Repository/Repositories/EmployeeRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.Data.Contexts;
+using Company.Data.Entites;
+using Company.Repository.Interfaces;
+
+namespace Company.Repository.Repositories
+{
+    public class EmployeeRepository : GenericRepository<Employee>, IEmployeeRepository
+    {
+        private readonly CompanyDbContext _context;
+
+        public EmployeeRepository(CompanyDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Employee> GetEmployeeByName(string name)
+            => _context.Employees.Where(x => x.Name.Trim().ToLower().Contains(name.Trim().ToLower())).ToList();
+
+        public IEnumerable<Employee> GetEmployeesByAddress(string address)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
