c776ffbb358c66a2d3c4c7538963a2c0d29ac3cf
Youssef El sherbiny <149201516+Elsherbiny73@users.noreply.github.com>
2025-04-10 03:38:27 +0200

    Add files via upload

---
+++ Route_Assignments/MVC_Session5/Company.Web/Models/ResetPasswordViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.Web.Models
+{
+    public class ResetPasswordViewModel
+    {
+
+        [Required(ErrorMessage = "Password is Required")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{6,}$", ErrorMessage = "Password must be at least 6 characters long and include uppercase, lowercase, digit, and non-alphanumeric character.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is Required")]
+        [Compare(nameof(Password), ErrorMessage = "Confirm Password does not match Password")]
+        public string ConfirmPassword { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
+    }
+}
