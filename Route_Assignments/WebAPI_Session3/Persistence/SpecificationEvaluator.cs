da5e7e70d3c4506cdfa49a652dc7da7291cb3d02
Youssef El sherbiny <149201516+Elsherbiny73@users.noreply.github.com>
2025-05-04 15:14:33 -0700

    Add files via upload

---
+++ Route_Assignments/WebAPI_Session3/Persistence/SpecificationEvaluator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public static class SpecificationEvaluator
+    {
+        public static IQueryable<T> GetQuery<T>(IQueryable<T> BaseQuery, Specification<T> specification) where T : class
+        {
+            var query = BaseQuery;
+            if (specification.Criteria is not null)
+                query = query.Where(specification.Criteria);
+
+            query = specification.Includes.Aggregate(query, (currentQuery, include) => currentQuery.Include(include));
+
+            if (specification.OrderBy is not null)
+                query = query.OrderBy(specification.OrderBy);
+
+            else if (specification.OrderByDescending is not null)
+                query = query.OrderBy(specification.OrderByDescending);
+
+            if (specification.IsPaginated)
+                query = query.Skip(specification.Skip).Take(specification.Take);
+
+            return query;
+        }
+    }
+}
---
+++ Route_Assignments/WebAPI_Session3/Presentation/Controllers/ApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class ApiController : ControllerBase
+    {
+
+    }
+}
